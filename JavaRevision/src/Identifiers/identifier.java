2dc7fb520be31f5fc0181076bb589757e9705207
Kavyashree A Baindoor <kavyashree.baindoor@ad.infosys.com>
2024-12-04 09:46:08 +0530

    identifier

---
+++ JavaRevision/src/Identifiers/identifier.java
@@ -0,0 +1,44 @@
+package Identifiers;
+
+public class identifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//can be method name,variable name ,class name any name
+		
+		//rules:
+		
+		//a to z   A to Z
+		// $
+		// _
+		// 0 to 9
+		
+		//can't start with digit  ---- 123total --noo
+		
+		//java language is case sensitive
+		
+		int number=10;
+		int NUMBER =20;
+		int Number=30;
+		
+		//all are different  -- so no error
+		
+		
+		//reserved words cannot be used like if,else
+		
+		int String =888;
+		System.out.println(String);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
