1aa733abe5cfe89665ee8bd2e3a4f691ab61e192
Kavyashree A Baindoor <kavyashree.baindoor@ad.infosys.com>
2024-12-07 13:10:38 +0530

    literals

--- JavaRevision/src/Introduction/Datatypes2.java
+++ JavaRevision/src/Introduction/Datatypes2.java
@@ -4,7 +4,34 @@ public class Datatypes2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+//		int x=0;
+//		
+//		if(x) {  //change type of x to boolean
+		
+		
+//			System.out.println("true");
+		
+//		}
+		
 
+	/////////////////////////////////////////////////////////////////////////////	
+		
+		
+//		while(1) { 
+		
+		//type mismatch cannot conevert from int to boolean
+		
+//			System.out.println("hi");
+//		}
+		
+		
+		
+	////////////////////////////////////////////////////////////	
+		
+		
+		
 	}
 
 }
